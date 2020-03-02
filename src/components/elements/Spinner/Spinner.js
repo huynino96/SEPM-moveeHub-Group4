412d1be92195a02f7b690b74148efc0a750a5c38
Huy Ho <tuanhuy0496@yahoo.com>
2020-03-02 16:46:29 +0700

    Add more elements  to Homepage

---
+++ src/components/elements/Spinner/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Spinner.css';
+
+const Spinnner = () => {
+    return(
+        <div className ="loader">
+
+        </div>
+    )
+}
+
+export default Spinnner;
