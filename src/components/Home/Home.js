6a1199c67da3179dcdbd9bc12825b369edeabd15
Huy Ho <tuanhuy0496@yahoo.com>
2020-03-04 16:33:16 +0700

    Search Function

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {API_URL, API_KEY, IMAGE_BASE_URL, POSTER_SIZE, BACKDROP_SIZE} from '../../config';
+import React, { Component } from 'react';
+import { API_URL, API_KEY, IMAGE_BASE_URL, POSTER_SIZE, BACKDROP_SIZE } from '../../config';
 import HeroImage from '../elements/HeroImage/HeroImage';
 import SearchBar from '../elements/SearchBar/SearchBar';
 import FourColGrid from '../elements/FourColGrid/FourColGrid';
@@ -8,8 +8,8 @@ import LoadMoreBtn from '../elements/LoadMoreBtn/LoadMoreBtn';
 import Spinner from '../elements/Spinner/Spinner';
 import './Home.css';
 
-class Home extends Component{
-    state ={
+class Home extends Component {
+    state = {
         movies: [],
         heroImage: null,
         loading: false,
@@ -19,18 +19,37 @@ class Home extends Component{
 
     }
 
-    componentDidMount(){
-        this.setState({loading: true});
+    componentDidMount() {
+        this.setState({ loading: true });
         const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+        this.fetchItems(endpoint);
+    }
+
+    searchItems = (searchTerm) => {
+        console.log(searchTerm);
+        let endpoint = '';
+        this.setState({
+            movies: [],
+            loading: true,
+            searchTerm
+        })
+        
+        if (searchTerm ==='') {
+            endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+
+        }
+        else {
+            endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}`;
+        }
         this.fetchItems(endpoint);
     }
 
     loadMoreItems = () => {
         let endpoint = '';
-        this.setState({loading: true});
+        this.setState({ loading: true });
 
-        if (this.state.searchTerm === ''){
-            endpoint =`${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${this.state.currentPage + 1}`;
+        if (this.state.searchTerm === '') {
+            endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${this.state.currentPage + 1}`;
 
         }
         else {
@@ -39,29 +58,36 @@ class Home extends Component{
         this.fetchItems(endpoint);
     }
 
-    fetchItems = (endpoint)=> {
+    fetchItems = (endpoint) => {
         fetch(endpoint)
-        .then(result => result.json())
-        .then(result => {
-            this.setState({
-                movies: [...this.state.movies, ...result.results],
-                heroImage: this.state.heroImage || result.results[0],
-                loading: false,
-                currentPage: result.page,
-                totalPages: result.total_pages,
+            .then(result => result.json())
+            .then(result => {
+                this.setState({
+                    movies: [...this.state.movies, ...result.results],
+                    heroImage: this.state.heroImage || result.results[0],
+                    loading: false,
+                    currentPage: result.page,
+                    totalPages: result.total_pages,
+                })
             })
-        })
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div className="rmdb-home">
-                <HeroImage />
-                <SearchBar />
+                {this.state.heroImage ?
+                <div>
+                    <HeroImage 
+                        image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${this.state.heroImage.backdrop_path}`}
+                        title={this.state.heroImage.original_title}
+                        text={this.state.heroImage.overview}
+                    />
+                    <SearchBar callback={this.searchItems} />
+                </div> : null}
                 <FourColGrid />
                 <Spinner />
                 <LoadMoreBtn />
-                
+
             </div>
         )
     }
