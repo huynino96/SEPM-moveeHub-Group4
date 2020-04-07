4c1bc0e77a55ab9e5cc8991af0450f68441ddbbb
Huy Ho <tuanhuy0496@yahoo.com.vn>
2020-04-07 15:46:49 +0700

    Header Logo Edit

--- src/components/elements/Header/Header.js
+++ src/components/elements/Header/Header.js
@@ -6,9 +6,9 @@ const Header = () => (
   <div className="rmdb-header">
     <div className="rmdb-header-content">
       <Link to="/">
-        <img className="rmdb-logo" src="/images/reactMovie_logo.png" alt="rmdb-logo" />
+        <img className="rmdb-logo" src="/images/logo3.png" alt="rmdb-logo" />
       </Link>
-      <img className="rmdb-tmdb-logo" src="/images/tmdb_logo.png" alt="tmdb-logo" />
+      {/*<img className="rmdb-tmdb-logo" src="/images/tmdb_logo.png" alt="tmdb-logo" />*/}
     </div>
   </div>
 )
