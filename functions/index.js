f407fdba21ca5ecf7ed36301e5a8ac61289d1ee0
Huy Ho <tuanhuy0496@yahoo.com>
2020-04-20 20:44:51 +0700

    Validate User Login

--- functions/index.js
+++ functions/index.js
@@ -66,6 +66,23 @@ app.post("/scream", (req, res) => {
     });
 });
 
+const isEmail = (email) => {
+  const regEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(regEx)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const isEmpty = (string) => {
+  if (string.trim() === '') {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 // Signup rout
 app.post("/signup", (req, res) => {
   const newUser = {
@@ -75,6 +92,29 @@ app.post("/signup", (req, res) => {
     handle: req.body.handle,
   };
 
+  let errors = {};
+
+  if (isEmpty(newUser.email)) {
+    errors.email = "Must not be empty";
+  } else if (!isEmail(newUser.email)) {
+    errors.email = "Must be a valid email address";
+  }
+
+  if (isEmpty(newUser.password)) {
+    errors.password = "Must not be empty";
+  }
+
+  if (newUser.password !== newUser.confirmPassword) {
+    errors.confirmPassword = "Password must be matched";
+  }
+
+  if (isEmpty(newUser.handle)) {
+    errors.handle = "Must not be empty";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return res.status(400).json(errors);
+  }
   // TODO: Validate data
 
   let token, userId;
@@ -94,7 +134,7 @@ app.post("/signup", (req, res) => {
       return data.user.getIdToken();
     })
     .then((idToken) => {
-    token = idToken;
+      token = idToken;
       const userCredentials = {
         handle: newUser.handle,
         email: newUser.email,
@@ -114,6 +154,40 @@ app.post("/signup", (req, res) => {
       } else {
         return res.status(500).json({ error: err.code });
       }
+    });
+});
+
+app.post("/login", (req, res) => {
+  const user = {
+    email: req.body.email,
+    password: req.body.password,
+  };
+
+  let errors = {};
+
+  if (isEmpty(user.email)) {
+    errors.email = "Must not be empty";
+  }
+  if (isEmpty(user.password)) {
+    errors.password = "Must not be empty";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return res.status(400).json(errors);
+  }
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(user.email, user.password)
+    .then((data) => {
+      return data.user.getIdToken();
+    })
+    .then((token) => {
+      return res.json({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ errror: err.code });
     });
 });
 
