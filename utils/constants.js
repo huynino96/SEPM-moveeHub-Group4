e0829c5267083054f4c6ba79fcb78faaf49742f2
deirpes <deirpes@gmail.com>
2020-05-07 17:47:37 +0700

    Add helper to get image for backdrop and poster

---
+++ utils/constants.js
@@ -0,0 +1,20 @@
+// Configuration for TMDB
+// To se the latest configuration fetch it from https://api.themoviedb.org/3/configuration?api_key=019e8f375549e0bbd4a4191862ebc88f
+export const API_URL = 'https://api.themoviedb.org/3';
+export const API_KEY = '844dba0bfd8f3a4f3799f6130ef9e335';
+export const API_LANGUAGE = 'en-us';
+
+// ENDPOINTS
+export const MOVIE_URL = '/movie/popular';
+export const SEARCH_URL = '/search/movie';
+
+// Images
+// An image URL looks like this example:
+// http://image.tmdb.org/t/p/w780/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg
+export const IMAGE_BASE_URL ='http://image.tmdb.org/t/p/';
+
+// Sizes: w300, w780, w1280, original
+export const BACKDROP_SIZE = 'w1280';
+
+// Sizes: w92, w154, w185, w342, w500, w780, original
+export const POSTER_SIZE = 'w500';
