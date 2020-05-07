cae36c8991bc325be1fbed7b95931273c8feb8af
deirpes <deirpes@gmail.com>
2020-05-07 17:48:49 +0700

    Add assets files for new layout

--- src/components/elements/NotFound/NotFound.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const NotFound = () => (
-  <div style={{ marginTop: '20rem', textAlign: 'center' }}>
-    <h1>Whooops. This page doesn't exist</h1>
-  </div>
-)
-
-export default NotFound;
