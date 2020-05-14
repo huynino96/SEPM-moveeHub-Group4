68fc4018447dc63339f6bd874089a50a7d08b807
deirpes <deirpes@gmail.com>
2020-05-14 23:29:54 +0700

    Add contact page

--- pages/contact.js
+++ pages/contact.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 import Backdrop from '../components/Backdrop';
 
-const Forum = () => <Backdrop title="A particular place to post, like, comment on" subtitle="Forum" imageUrl="/images/hero-2.jpg" />;
+const Contact = () => <Backdrop title="A particular place to post, like, comment on" subtitle="Forum" imageUrl="/images/hero-2.jpg" />;
 
-export default Forum;
+export default Contact;
