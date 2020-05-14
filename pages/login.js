3f9d9fd0034fcd600a0a2178ef86c085b85689a3
deirpes <deirpes@gmail.com>
2020-05-15 01:13:36 +0700

    Change text of login and register page

--- pages/login.js
+++ pages/login.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import Backdrop from '../components/Backdrop';
 
-const Login = () => <Backdrop title="Write write? Let's" subtitle="Login" imageUrl="/images/hero-3.jpg" />;
+const Login = () => (
+    <Backdrop title="Write write? Let's" subtitle="Login" imageUrl="/images/hero-3.jpg" />
+);
 
 export default Login;
--- pages/register.js
+++ pages/register.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 import Backdrop from '../components/Backdrop';
 
-const Register = () => <Backdrop title="Ready to write review, let's" subtitle="Register" imageUrl="/images/hero-4.jpg" />;
+const Register = () => <Backdrop title="Don't have account? Let's" subtitle="Register" imageUrl="/images/hero-4.jpg" />;
 
 export default Register;
